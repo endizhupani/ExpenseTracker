a6aecca4da41001b78a26070bacccd0ece25f48c
Endi Zhupani <endizhupani@gmail.com>
2021-04-10 00:17:50 +0200

    started adding domain behavior to the csv file validator

---
+++ ExpenseTracker.Domain.Tests/Entities/Configuration/CsvConfigAggregate/CsvFileConfigurationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Domain.Tests.Entities.Configuration.CsvConfigAggregate
+{
+    
+    public class CsvFileConfigurationTests
+    {
+        //public void     
+    }
+}
--- src/Core/ExpenseTracker.Domain/Entities/Configuration/CsvConfigAggregate/CsvFileColumn.cs
+++ src/Core/ExpenseTracker.Domain/Entities/Configuration/CsvConfigAggregate/CsvFileColumn.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTracker.Domain.Common;
+﻿using Ardalis.GuardClauses;
+using ExpenseTracker.Domain.Common;
 
 namespace ExpenseTracker.Domain.Entities.Configuration.BankAggregate
 {
@@ -7,17 +8,38 @@ namespace ExpenseTracker.Domain.Entities.Configuration.BankAggregate
     /// </summary>
     public class CsvFileColumn:Entity
     {
+        private CsvFileColumn()
+        {
+            
+        }
+
+        public CsvFileColumn(TransactionRecordColumn transactionRecordColumn, string name, string valueFormatString, int position)
+        {
+            Guard.Against.NullOrEmpty(name, parameterName: nameof(name));
+            Guard.Against.NullOrEmpty(valueFormatString, parameterName: nameof(valueFormatString));
+
+            TransactionRecordColumn = transactionRecordColumn;
+            if (transactionRecordColumn.Id > 0)
+            {
+                TransactionRecordColumnId = transactionRecordColumn.Id;
+            }
+
+            Name = name;
+            ValueFormatString = valueFormatString;
+            Position = position;
+        }
+
         /// <summary>
         /// Name of the column
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
-        public int Position { get; set; }
+        public int Position { get; private set; }
 
-        public string ValueFormatString { get; set; }
+        public string ValueFormatString { get; private set; }
 
-        public int TransactionRecordColumnId { get; set; }
-        public virtual TransactionRecordColumn TransactionRecordColumn { get; set; }
+        public int TransactionRecordColumnId { get; private set; }
+        public virtual TransactionRecordColumn TransactionRecordColumn { get; private set; }
 
     }
 }
